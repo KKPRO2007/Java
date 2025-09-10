7f3344a00c156cba67be5d16f405701c8977eee9
kk professional <kkpro2007@gmail.com>
2025-09-10 09:03:20 +0530

    Code

--- Practice/String/Toggle_case_of_each_char.java
+++ Practice/String/Toggle_case_of_each_char.java
@@ -1,21 +1,21 @@
 package String;
 import java.util.Scanner;
-public class Toggle_case_of_each_char{
-	public static void main(String[] args) {
-	Scanner z = new Scanner ( System.in);
-	System.out.println("Entre the string: ");
-    String s = z.nextLine();
-    String toggle= "";
-    for (int i = 0; i < s.length(); i++) {
-    	char c = s.charAt(i);
-    	if (Character.isUpperCase(c))
-    		toggle= toggle + Character.toLowerCase(c);
-    	else if (Character.isLowerCase(c)) 
-            toggle= toggle + Character.toUpperCase(c);
-    	else 
-            toggle= toggle + c;
-    }
-    System.out.println("Toggled: "+ toggle);
 
-}
+public class Toggle_case_of_each_char {
+    public static void main(String[] args) {
+        Scanner z = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String s1 = z.nextLine();
+        String s2 = "";
+        for (int i = 0; i < s1.length(); i++) {
+            char c = s1.charAt(i);
+            if (c >= 'A' && c <= 'Z')
+                s2 = s2 + (char)(c + 32);  
+            else if (c >= 'a' && c <= 'z')
+                s2 = s2 + (char)(c - 32);  
+            else
+                s2 = s2 + c;  
+        }
+        System.out.println("Toggled: " + s2);
+    }
 }
